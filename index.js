7193292eafb47cfe0ed0b7350df3272fe3d309b2
alemranakash <alemranexpert@gmail.com>
2023-12-21 19:49:34 +0600

    Implemented get and post method for Tasks

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+//* Middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ftdzayi.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const taskCollection = client.db("taskDB").collection("tasks");
+
+// * task related apis
+app.post('/tasks', async (req, res) => {
+  const item = req.body;
+  const result = await taskCollection.insertOne(item);
+  res.send(result);
+});
+
+
+app.get('/tasks', async (req, res) => {
+  const result = await taskCollection.find().toArray();
+  res.send(result);
+});
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+//to check server is running
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
