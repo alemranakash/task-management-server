f176bade1aed5ff0ae5114f28c4e0dc473992b94
alemranakash <alemranexpert@gmail.com>
2023-12-22 12:10:49 +0600

    Added delete operation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ftdzayi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,6 +42,15 @@ app.get('/tasks', async (req, res) => {
   const result = await taskCollection.find().toArray();
   res.send(result);
 });
+
+
+app.delete('/tasks/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const result = await taskCollection.deleteOne(query);
+  res.send(result);
+})
+
 
 
     // Send a ping to confirm a successful connection
